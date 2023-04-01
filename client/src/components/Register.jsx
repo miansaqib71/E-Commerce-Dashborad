5ad20934bfc6098dde490f750958669955ecb06d
Muhammad Saqib <74395346+miansaqib71@users.noreply.github.com>
2023-04-02 02:21:50 +0500

    Api integrate  cors issue fix or async await  or use redirect use

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,19 +1,32 @@
 import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
 
 const Register = () => {
-    const [name , setName] = useState("")
-    const [email , setEmail] = useState("")
-    const [password , setPassword] = useState("")
-    const collectData=()=>{
-        console.log(name, email, password)
-    }
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const redirct = useNavigate()
+
+  const collectData = async () => {
+    console.log(name, email, password);
+    let data = await fetch("http://localhost:5000/register",{
+      method:"post",
+      body:JSON.stringify({name,email,password}),
+      headers:{
+        "Content-Type":"application/json",
+      }
+    });
+    data = await data.json();
+    console.log(data)
+    redirct("/home")
+  }
   return (
-<div className='register_screen'>
-<input className='input_data' type={'text'} value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter Name" />
-<input className='input_data' type={'text'} value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter email" />
-<input className='input_data' type={'password'} value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter password" />
-<button className='register_btn' onClick={collectData} type='button' >Register</button>
-</div>  )
+    <div className='register_screen'>
+      <input className='input_data' type={'text'} value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter Name" />
+      <input className='input_data' type={'text'} value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email" />
+      <input className='input_data' type={'password'} value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" />
+      <button className='register_btn' onClick={collectData} type='button' >Register</button>
+    </div>)
 }
 
 export default Register
