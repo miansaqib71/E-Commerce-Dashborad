18e5b70b17262a1496942b2c07bc3ee11e981544
Muhammad Saqib <74395346+miansaqib71@users.noreply.github.com>
2023-04-01 14:35:16 +0500

    cors update install and use backend node js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+var cors = require('cors')
 require("./dbs/Config")
 const User = require("./dbs/User")
 
 const app = express()
 app.use(express.json());
+app.use(cors())
 
 app.post("/register", async(req, res) => {
     let data = new User(req.body);
