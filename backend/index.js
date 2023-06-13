a12db37333b975310edec75cc6f823229092534c
Muhammad Saqib <74395346+miansaqib71@users.noreply.github.com>
2023-06-14 01:06:49 +0300

    Verify js token
    Verify js token in node js use different API add new function

--- backend/index.js
+++ backend/index.js
@@ -17,12 +17,12 @@ app.post("/register", async (req, res) => {
     // console.log(userData)
     userData = userData.toObject();
     delete data.password
-    jwt.sign({userData}, privateKey, { expiresIn: '24h' }, function(err, token) {
-        if(err){
-         res.send({result:"Something wrong"})
+    jwt.sign({ userData }, privateKey, { expiresIn: '24h' }, function (err, token) {
+        if (err) {
+            res.send({ result: "Something wrong" })
         }
-        res.send({userData, auth:token})
-       });
+        res.send({ userData, auth: token })
+    });
     // res.send(userData)
 })
 
@@ -30,12 +30,12 @@ app.post("/login", async (req, res) => {
     if (req.body.password && req.body.email) {
         let user = await User.findOne(req.body).select("-password")
         if (user) {
-            jwt.sign({user}, privateKey, { expiresIn: '24h' }, function(err, token) {
-               if(err){
-                res.send({result:"Something wrong"})
-               }
-               res.send({user, auth:token})
-              });
+            jwt.sign({ user }, privateKey, { expiresIn: '24h' }, function (err, token) {
+                if (err) {
+                    res.send({ result: "Something wrong" })
+                }
+                res.send({ user, auth: token })
+            });
         } else {
             res.send({ data: "data Not found" })
         }
@@ -43,12 +43,12 @@ app.post("/login", async (req, res) => {
         res.send({ data: "data Not found" })
     }
 })
-app.post("/add-product", async (req, res) => {
+app.post("/add-product", tokenVerify,async (req, res) => {
     let productData = new Product(req.body);
     let data = await productData.save();
     res.send(data)
 })
-app.get("/products", async (req, res) => {
+app.get("/products", tokenVerify,async (req, res) => {
     const products = await Product.find();
     if (products.length > 0) {
         res.send(products)
@@ -56,40 +56,58 @@ app.get("/products", async (req, res) => {
         res.send({ data: "No Data Found" })
     }
 })
-app.delete("/product/:id", async (req, res) => {
+app.delete("/product/:id",tokenVerify, async (req, res) => {
     // res.send("app is working")
     let data = await Product.deleteOne({ _id: req.params.id })
     res.send(req.params)
 })
-app.get("/product/:id", async (req, res) => {
+app.get("/product/:id",tokenVerify, async (req, res) => {
     let data = await Product.findOne({ _id: req.params.id })
     if (data) {
         res.send(data)
     } else {
-        res.send({ "data":"data not found"})
+        res.send({ "data": "data not found" })
     }
 })
-app.put("/product/:id", async(req,res)=>{
-    let data = await Product.updateOne({_id:req.params.id},
-        {$set:req.body})
-        res.send(data)
+app.put("/product/:id", tokenVerify, async (req, res) => {
+    let data = await Product.updateOne({ _id: req.params.id },
+        { $set: req.body })
+    res.send(data)
 })
 
-app.get("/search/:key", async(req,res)=>{
+app.get("/search/:key", tokenVerify, async (req, res) => {
     let data = await Product.find({
-        "$or":[
+        "$or": [
             {
-                name:{$regex:req.params.key}
+                name: { $regex: req.params.key }
             },
             {
-                category:{$regex:req.params.key}
+                category: { $regex: req.params.key }
             },
             {
-                company:{$regex:req.params.key}
+                company: { $regex: req.params.key }
             }
         ]
     })
     res.send(data)
 })
+
+function tokenVerify(req, res, next) {
+    let token = req.headers["authorization"]
+    if (token) {
+        token = token.split(" ")[1]
+        console.log("Middler ware Called", token)
+        jwt.verify(token, privateKey, (err, valid) => {
+            if (err) {
+                res.status(401).send({ result: "Please add valid Token" })
+            } else {
+                next()
+            }
+        })
+    } else {
+        res.status(403).send({ result: "Please add Tokken With header" })
+    }
+
+}
 
 app.listen(5000)
